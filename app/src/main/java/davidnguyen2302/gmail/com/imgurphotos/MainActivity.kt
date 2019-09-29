16483278977c60f88611cd1d568c68164bbd1cdf
davidnguyen2302 <david.nguyen.2302@gmail.com>
2019-09-28 23:25:44 -0700

    Test search field
    Check if the search field can get the correct text input

--- app/src/main/java/davidnguyen2302/gmail/com/imgurphotos/MainActivity.kt
+++ app/src/main/java/davidnguyen2302/gmail/com/imgurphotos/MainActivity.kt
@@ -2,19 +2,97 @@ package davidnguyen2302.gmail.com.imgurphotos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.SearchView
 import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private val TAG by lazy { MainActivity::class.java.simpleName }
+    }
+    private val baseUrl: String = "https://api.imgur.com/3/gallery/search/time/"
+    private val tempUrl: String = "https://api.imgur.com/3/gallery/search/time/1?q=cats"
+    private var results: String? = ""
+
+    /**
+     * Create the view of the Main page of the app
+     * @param savedInstanceState is the state of the instance of the app
+     * null at first run, and it will get used when the activity state changed
+     * (e.g. orientation changed)
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+//        val client = OkHttpClient()
+//
+//        val request = Request.Builder()
+//                .url(tempUrl)
+//                .get()
+//                .addHeader("authorization", "Client-ID 126701cd8332f32")
+//                .build()
+//
+//        val response = client.newCall(request).enqueue(object: Callback {
+//            override fun onFailure(call: Call, e: IOException) {
+//                e.stackTrace
+//            }
+//
+//            override fun onResponse(call: Call, response: Response) {
+//                result = response.body?.string()
+//                runOnUiThread {
+//                    textView.text = result
+//                }
+//            }
+//        })
+
+
+
+
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        //Inflate tehe menu to use in the action bar
+    /**
+     * Create searchView and inflate the menu
+     * @param menu is the menu item
+     * @return return true to add this menu always to MainActivity
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //Inflate the menu to use in the action bar
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_search, menu)
-        return super.onCreateOptionsMenu(menu)
+
+        createSearchView(menu)
+
+        return true
     }
+
+    /**
+     * Helper method to create the Search field in the menu bar
+     * @param menu is the searchView menu item
+     */
+    private fun createSearchView(menu: Menu) {
+        val searchItem: MenuItem = menu.findItem(R.id.action_search)
+        val searchView: SearchView = searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                runOnUiThread {
+                    textView.text = query
+                }
+                return true
+            }
+        })
+    }
+
 }
