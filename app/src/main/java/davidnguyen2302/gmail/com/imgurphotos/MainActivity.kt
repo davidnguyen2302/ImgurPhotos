4d061c301e85adf2d8e053318c7fa495b04a3297
davidnguyen2302 <david.nguyen.2302@gmail.com>
2019-09-29 13:57:54 -0700

    Get images' ids and titles
    Get a correct response from get request

--- app/src/main/java/davidnguyen2302/gmail/com/imgurphotos/MainActivity.kt
+++ app/src/main/java/davidnguyen2302/gmail/com/imgurphotos/MainActivity.kt
@@ -3,21 +3,29 @@ package davidnguyen2302.gmail.com.imgurphotos
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.SearchView
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_photo.*
 import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.IOException
 
-
+/**
+ * Home page class with search menu and view to display the images
+ * @author Duc Nguyen
+ */
 class MainActivity : AppCompatActivity() {
 
     companion object {
         private val TAG by lazy { MainActivity::class.java.simpleName }
     }
-    private val baseUrl: String = "https://api.imgur.com/3/gallery/search/time/"
-    private val tempUrl: String = "https://api.imgur.com/3/gallery/search/time/1?q=cats"
+
+    private val baseUrl: String = "https://api.imgur.com/3/gallery/search/time/1?q="
     private var results: String? = ""
+    private var gallery: String = "" //TODO: Remove after testing
 
     /**
      * Create the view of the Main page of the app
@@ -28,34 +36,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
-//        val client = OkHttpClient()
-//
-//        val request = Request.Builder()
-//                .url(tempUrl)
-//                .get()
-//                .addHeader("authorization", "Client-ID 126701cd8332f32")
-//                .build()
-//
-//        val response = client.newCall(request).enqueue(object: Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                e.stackTrace
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                result = response.body?.string()
-//                runOnUiThread {
-//                    textView.text = result
-//                }
-//            }
-//        })
-
-
-
-
 
     }
 
@@ -81,18 +61,59 @@ class MainActivity : AppCompatActivity() {
     private fun createSearchView(menu: Menu) {
         val searchItem: MenuItem = menu.findItem(R.id.action_search)
         val searchView: SearchView = searchItem.actionView as SearchView
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextChange(newText: String?): Boolean {
                 return false
             }
 
             override fun onQueryTextSubmit(query: String?): Boolean {
-                runOnUiThread {
-                    textView.text = query
-                }
+                val url = baseUrl.plus(query)
+                getRequest(url)
                 return true
             }
         })
     }
 
+    /**
+     * Create the GET request using the search input
+     */
+    private fun getRequest(url: String) {
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("authorization", "Client-ID 126701cd8332f32")
+                .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e(TAG, "An error has occurred ${e.stackTrace}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val data = JSONObject(response.body?.string())
+                if (data != null) {
+                    val items: JSONArray = data.getJSONArray("data")
+                    val photos = arrayListOf<Photo>()
+
+                    for (i in 0 until items.length()) {
+                        var photo: Photo = object : Photo() {}
+                        val item: JSONObject = items.getJSONObject(i)
+                        photo.id = item.getString("id")
+                        photo.title = item.getString("title")
+
+                        photos.add(photo)
+                    }
+                    Log.d(TAG, "IMAGES SIZE: ${photos.size}")
+                    for (i in 0 until photos.size) {
+                        println("IMAGES $i: ${photos[i].id}, ${photos[i].title}")
+                    }
+                } else {
+                    Log.d(TAG, "$data is null")
+                }
+            }
+        })
+
+    }
 }
---
+++ app/src/main/java/davidnguyen2302/gmail/com/imgurphotos/Photo.kt
@@ -0,0 +1,6 @@
+package davidnguyen2302.gmail.com.imgurphotos
+
+open class Photo {
+    var id: String? = ""
+    var title: String? = ""
+}
